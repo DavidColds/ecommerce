031190fcb9f9a43acc75210d8a4ae7dcf3878dd6
David Drughi <drughidavid6@gmail.com>
2019-10-02 00:26:55 +0200

    products page with linking and products

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, product_detail, all_products2
+
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^$', all_products2, name='all_products2'),
+    url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from posts.models import Post
+from django.utils import timezone
 
-# Create your views here.
+
 def all_products(request):
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, "products.html", {"products": products, 'posts': posts})
+    
+def all_products2(request):
+    products = Product.objects.all()
+    return render(request, "allproducts.html", {"products": products})
+    
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.save()
+    return render(request, "productdetail.html", {'product': product})
+    
+def get_posts2(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, "products.html", {'posts': posts})
