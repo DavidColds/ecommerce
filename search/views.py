b75dd76f8818aa14ca7da0304f74a647de809fe5
David Drughi <drughidavid6@gmail.com>
2019-10-02 00:33:39 +0200

    search form working

--- search/views.py
+++ search/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from products.models import Product
 
-# Create your views here.
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    products = Product.objects.filter(name__contains=request.GET['q'])
+    return render(request, "allproducts.html", {"products": products})
