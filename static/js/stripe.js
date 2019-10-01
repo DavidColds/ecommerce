ef6259f768be00659723e6b0f96747d69066914a
David Drughi <drughidavid6@gmail.com>
2019-10-02 00:34:51 +0200

    style added to css and stripe

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,20 +1,18 @@
 $(function() {
     $("#payment-form").submit(function() {
         var form = this;
-        var card = {
-            number: $("#id_credit_card_number").val(),
-            expMonth: $("#id_expiry_month").val(),
-            expYear: $("#id_expiry_year").val(),
-            cvc: $("#id_cvv").val()
-        };
+        Stripe.card.createToken({
+            number: $('.card-number').val(),
+            cvc: $('.card-cvc').val(),
+            exp_month: $('.card-expiry-month').val(),
+            exp_year: $('.card-expiry-year').val()
+          }, stripeResponseHandler);
     
     Stripe.createToken(card, function(status, response) {
         if (status === 200) {
             $("#credit-card-errors").hide();
             $("#id_stripe_id").val(response.id);
 
-            // Prevent the credit card details from being submitted
-            // to our server
             $("#id_credit_card_number").removeAttr('name');
             $("#id_cvv").removeAttr('name');
             $("#id_expiry_month").removeAttr('name');
