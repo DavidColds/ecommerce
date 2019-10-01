40ea885d32954f31b51e077be86db50e0023f19e
David Drughi <drughidavid6@gmail.com>
2019-10-01 13:21:59 +0200

    home page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,36 +1,43 @@
-"""ecommerce URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from products.views import all_products2
+from products.views import all_products
+from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
 from checkout import urls as urls_checkout
-from products.views import all_products
 from django.views import static
+from django.views.generic import RedirectView
+from posts import urls as urls_posts
+from posts.views import get_posts
+from suggestions import urls as urls_suggestions
+from suggestions.views import get_suggestions
+from django.views.static import serve
 from .settings import MEDIA_ROOT
+from home.views import about
+from contact.views import contact
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
+    url(r'^products/', include('products.urls')),
+    url(r"^products_list/$", all_products2, name="all_products2"),
     url(r'^cart/', include(urls_cart)),
-    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    url(r'^checkout/', include(urls_checkout)),
+    url(r'^posts/', include('posts.urls')),
+    url(r'^posts/', include(urls_posts)),
+    url(r"^posts$", get_posts, name="get_posts"),
+    url(r'^suggestions/', include('suggestions.urls')),
+    url(r'^suggestions/', include(urls_suggestions)),
+    url(r"^suggestions$", get_suggestions, name="get_suggestions"),
+    url('contact_page/', contact, name='contact'),
+    url('about/', about, name='about'),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    """A view that displays the index page"""
-    return render(request, "index.html")
+def about(request):
+    """View to display about.html"""
+    return render(request, "about.html")
+    
+def contact(request):
+    """View to display about.html"""
+    return render(request, "contact.html")
