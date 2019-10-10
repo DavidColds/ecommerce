8d5872fea3ab98f94d29f83ea3453bed464d1f87
David Drughi <drughidavid6@gmail.com>
2019-10-10 18:04:24 +0200

    added allowed hosts for heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://winebordeaux.herokuapp.com/']
+ALLOWED_HOSTS = ['winebordeaux.herokuapp.com']
 
 
 
